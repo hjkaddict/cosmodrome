a83d3d66a5e6baf6abf6118a15a6e33831a97731
hjkaddict <hjkaddcit@gmail.com>
2020-06-08 20:18:43 +0200

    =a

--- app.js
+++ app.js
@@ -9,24 +9,24 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
-const fileList = [];
-const getAllFiles = function (dirPath, fileList) {
-    files = fs.readdirSync(dirPath).filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));;
-    files.forEach((value, index) => {
-        subfiles = fs.readdirSync(dirPath + '/' + value);
-        fileList.push({
-            name: value,
-            files: subfiles
-        })
-    })
-    return fileList;
-}
+// const fileList = [];
+// const getAllFiles = function (dirPath, fileList) {
+//     files = fs.readdirSync(dirPath).filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));;
+//     files.forEach((value, index) => {
+//         subfiles = fs.readdirSync(dirPath + '/' + value);
+//         fileList.push({
+//             name: value,
+//             files: subfiles
+//         })
+//     })
+//     return fileList;
+// }
 
-console.log(getAllFiles("public/p5js", fileList));
+// console.log(getAllFiles("public/p5js", fileList));
 
 app.get('/', (req, res) => {
     res.render('index', {
-        fileList: fileList
+        // fileList: fileList
     })
 })
 
